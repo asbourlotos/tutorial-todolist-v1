7ba88e31d965862b82eb05dfdd0f4b88566363d1
Alexander Bourlotos <alexanderbourlotos@alexanders-mbp.lan>
2022-06-15 00:37:44 -0500

    265 - half complete. Sleep time.

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.get("/", function(req, res) {
+    var today = new Date();
+    var currentDay = today.getDay();
+
+    if (currentDay === 6 || currentDay === 0) {
+        res.send("Yay it's the weekend!");
+    } else {
+        res.send("Boo! I have to work!");
+    }
+});
+
+
+
+app.listen(3000, function() {
+    console.log("Server started on port 3000");
+});
